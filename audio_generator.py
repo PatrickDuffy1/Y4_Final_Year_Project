46f89b9cea4bfd5ec0b6e98ca8f1f37df3996944
Patrick Duffy <G00408635@atu.ie>
2024-11-26 21:14:35 +0000

    Implemented basic TTS functionality

--- audio_generator.py
+++ audio_generator.py
@@ -1,7 +1,22 @@
-def test_function(text, file, voice):
-    print("Test function called")
-    print("Using text:", text)
-    print("Using file:", file)
-    print("Using voice:", voice)
+import torch
+from TTS.api import TTS
+
+DEFAULT_OUTPUT_FILE_PATH = "./outputs/output.wav"
+
+def generate_audio(text, file, voice):
+
+    output_file_path = DEFAULT_OUTPUT_FILE_PATH
     
-    return ""
+    # Use cuda if available, otherwise use the cpu
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    print("Using device:", device)
+    
+    # Load TTS model
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+        
+    # Run TTS
+    tts.tts_to_file(text, speaker_wav=voice, language="en", file_path=output_file_path)
+    
+    # Return the path to the created audio file
+    return output_file_path
--- ui.py
+++ ui.py
@@ -1,24 +1,24 @@
 import gradio as gr
-from audio_generator import test_function
+from audio_generator import generate_audio
 
 # Generate the audio using the chosen text/file, and the chosen voice
-def generate_audio(text, file, voice):
+def gradio_generate_audio(text, file, voice):
     
-    test_function(text, file, voice)
+    audio = generate_audio(text, file, voice)
 
     return audio
     
 
 demo = gr.Interface(
-    fn=generate_audio,
+    fn=gradio_generate_audio,
     inputs=[
         "text", # Input box for text
         "file", # Input box for files
         # Dropdown menu for voices
         gr.Dropdown(
-                choices=["./voice_1.wav", "./voice_2.wav"],
+                choices=["./voices/voice_1.wav", "./voices/voice_2.wav"],
                 label="Voice", 
-                value="./voice_1.wav" # Default voice
+                value="./voices/voice_1.wav" # Default voice
             ),
         ],
     outputs=["audio"], # Output box for audio file
