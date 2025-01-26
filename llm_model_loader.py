e70675ef226b68dd51ad70dafd342b7df9c4ee72
Patrick Duffy <G00408635@atu.ie>
2025-01-26 13:14:51 +0000

    Added code to load an LLM that was manually downloaded

--- llm_model_loader.py
+++ llm_model_loader.py
@@ -1,2 +1,24 @@
-def load_llm_model():
-    return None
+from llama_cpp import Llama
+
+# Load LLM model
+def load_llm_model(model_path, load_from_huggingface):
+    
+    llm = None
+    
+    # If the model was manually downloaded, load it based on its file path
+    if load_from_huggingface is False:
+        llm = load_model_from_file(model_path)
+    
+    return llm
+    
+
+# Load a model from a file path
+def load_model_from_file(model_path):
+
+    llm = Llama(
+          model_path=model_path, # Path to model
+          n_gpu_layers=1, # Layers to offload to GPU
+          n_ctx=8192, # Context length
+    )
+    
+    return llm
--- text_generator.py
+++ text_generator.py
@@ -1,3 +1,3 @@
 from llm_model_loader import load_llm_model
 
-print(load_llm_model())
+load_llm_model("./models/DeepSeek-R1-Distill-Llama-8B-Q4_K_M.gguf", False)
