078a89043f90d5f0260ef3a3cfa01f875ed2fc4d
Patrick Duffy <G00408635@atu.ie>
2025-01-26 14:03:49 +0000

    Added code to load an LLM from HuggingFace

--- llm_model_loader.py
+++ llm_model_loader.py
@@ -1,24 +1,43 @@
 from llama_cpp import Llama
 
 # Load LLM model
-def load_llm_model(model_path, load_from_huggingface):
+def load_llm_model(model_info):
     
     llm = None
     
     # If the model was manually downloaded, load it based on its file path
-    if load_from_huggingface is False:
-        llm = load_model_from_file(model_path)
+    if model_info["repo_id"] is None:
+        llm = load_model_from_file(model_info)
+    
+    # Load model from Hugging Face
+    else:
+        llm = load_model_from_huggingface(model_info)
     
     return llm
     
 
 # Load a model from a file path
-def load_model_from_file(model_path):
+def load_model_from_file(model_info):
 
     llm = Llama(
-          model_path=model_path, # Path to model
-          n_gpu_layers=1, # Layers to offload to GPU
-          n_ctx=8192, # Context length
+        model_path = model_info["file_name"], # Path to model
+        n_gpu_layers = model_info["n_gpu_layers"],
+        n_ctx = model_info["n_ctx"]
+    )
+    
+    return llm
+    
+
+# Load model from Hugging Face
+def load_model_from_huggingface(model_info):
+
+    from llama_cpp import Llama
+
+    llm = Llama.from_pretrained(
+        repo_id = model_info["repo_id"],
+        filename = model_info["file_name"],
+        n_gpu_layers = model_info["n_gpu_layers"],
+        n_ctx = model_info["n_ctx"]
     )
     
     return llm
--- text_generator.py
+++ text_generator.py
@@ -1,3 +1,21 @@
 from llm_model_loader import load_llm_model
 
-load_llm_model("./models/DeepSeek-R1-Distill-Llama-8B-Q4_K_M.gguf", False)
+# The following code is currently for testing purposes only
+
+test_model_info = {
+    "file_name": "./models/DeepSeek-R1-Distill-Llama-8B-Q4_K_M.gguf",
+    "repo_id": None,
+    "n_gpu_layers": 1, # Layers to offload to GPU
+    "n_ctx": 8192 # Context length
+}
+
+load_llm_model(test_model_info)
+
+test_model_info = {
+    "file_name": "Llama-3.2-3B-Instruct-Q4_K_M.gguf",
+    "repo_id": "bartowski/Llama-3.2-3B-Instruct-GGUF",
+    "n_gpu_layers": 1, # Layers to offload to GPU
+    "n_ctx": 8192 # Context length
+}
+
+load_llm_model(test_model_info)
