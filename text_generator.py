55ae63fb468af31f18319cc711fa1362f5f9dcde
Patrick Duffy <G00408635@atu.ie>
2025-01-27 17:32:08 +0000

    Implemented very basic LLM inference

--- text_generator.py
+++ text_generator.py
@@ -1,4 +1,26 @@
 from llm_model_loader import load_llm_model
+import json
+
+# Generate response based on an initial input
+def generate_text(initial_prompt, llm):
+
+    response = llm.create_chat_completion(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that outputs in JSON.",
+            },
+            {"role": "user", "content": "Who won the world series in 2020"},
+        ],
+        response_format={
+            "type": "json_object",
+        },
+        temperature=0.7,
+    )
+    
+    return response
+    
+
 
 # The following code is currently for testing purposes only
 
@@ -9,7 +31,7 @@ test_model_info = {
     "n_ctx": 8192 # Context length
 }
 
-load_llm_model(test_model_info)
+#load_llm_model(test_model_info)
 
 test_model_info = {
     "file_name": "Llama-3.2-3B-Instruct-Q4_K_M.gguf",
@@ -18,4 +40,12 @@ test_model_info = {
     "n_ctx": 8192 # Context length
 }
 
-load_llm_model(test_model_info)
+llm = load_llm_model(test_model_info)
+response = generate_text("", llm)
+
+# Extract the answer from the response
+content = response['choices'][0]['message']['content']
+result_dict = json.loads(content)
+result = result_dict['result']
+
+print(result)
