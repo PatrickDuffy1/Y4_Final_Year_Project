316ec41e89d3de30005d8bc5b48d1d89a2c6a10d
Patrick Duffy <G00408635@atu.ie>
2025-02-22 11:59:17 +0000

    Changed code to split chapters into chunks for the character line identification

--- modules/llm_line_identifier.py
+++ modules/llm_line_identifier.py
@@ -11,8 +11,41 @@ def identify_lines_in_chapter(chapter, llm):
         
     user_query = "Identify all of the lines in the text that are spoken by a character, and the character who spoke the line.\nDo not include any lines by the narrator.\n. Do not skip any lines spoken by a character.\nDo not make up any line that does not exist.\nThe line should be the full line, not just part of it. Identify the lines in order:\n\n"
     
-    temp = generate_json_text(user_query + chapter, llm, schema)['choices'][0]['message']['content']
+    #temp = generate_json_text(user_query + chapter, llm, schema)['choices'][0]['message']['content']
     
-    print("Temp", temp)
+    chunks = split_text_into_chunks(chapter, 5000)
+    identified_chunks = []
     
-    return temp
+    for chunk in chunks:
+        identified_chunks.append(generate_json_text(user_query + chunk, llm, schema)['choices'][0]['message']['content'])
+    
+    combined_lines = []
+    
+    for chunk in identified_chunks:
+        data = json.loads(chunk)
+        combined_lines.extend(data["lines"])
+        
+    combined_data = {"lines": combined_lines}
+
+    combined_json = json.dumps(combined_data, indent=4)
+
+    print(combined_json)
+    
+    return combined_json
+
+
+def split_text_into_chunks(text, chunk_size):
+
+    text = text.split("\n\n")
+    chunks = [""]
+    counter = 0
+    
+    for paragraph in text:
+    
+        if len(paragraph) + len(chunks[counter]) < chunk_size:
+            chunks[counter] += " " + paragraph
+        else:
+            counter += 1
+            chunks.append("")
+        
+    return chunks
