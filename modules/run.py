62987c546fd0d2e9c6e39fc398d9e4f5ec7def3a
Patrick Duffy <G00408635@atu.ie>
2025-02-28 14:58:03 +0000

    Ensure that outputs folder is created if it does not exist

--- modules/run.py
+++ modules/run.py
@@ -1,5 +1,14 @@
+import os
 from ui import Ui
 from session import Session
+
+# Get the parent directory of the script's location
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+outputs_path = os.path.join(parent_dir, "outputs")
+
+# Check if the outputs folder exists, and create it if not
+if not os.path.exists(outputs_path):
+    os.makedirs(outputs_path)
 
 current_session = Session()
 ui = Ui(current_session)
